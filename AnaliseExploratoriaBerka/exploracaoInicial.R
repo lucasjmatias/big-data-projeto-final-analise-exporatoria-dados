1354ea7550ef909b05c1f8c0b81d7518385fb1be
DESKTOP-HNQ5TBP\lucas <lucas.matias.87@gmail.com>
2018-09-28 16:55:21 -0300

    exploração inicial

---
+++ AnaliseExploratoriaBerka/exploracaoInicial.R
@@ -0,0 +1,14 @@
+read.csv2("./dados/client.asc", stringsAsFactors = FALSE) -> client
+read.csv2("./dados/account.asc", stringsAsFactors = FALSE) -> account
+read.csv2("./dados/card.asc", stringsAsFactors = FALSE) -> card
+read.csv2("./dados/disp.asc", stringsAsFactors = FALSE) -> disp
+read.csv2("./dados/district.asc", stringsAsFactors = FALSE) -> district
+read.csv2("./dados/loan.asc", stringsAsFactors = FALSE) -> loan
+read.csv2("./dados/order.asc", stringsAsFactors = FALSE) -> order
+read.csv2("./dados/trans.asc", stringsAsFactors = FALSE) -> trans
+
+
+
+
+
+View(client)
