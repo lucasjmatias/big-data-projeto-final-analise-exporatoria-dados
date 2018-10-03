8df1b8c1d9f72ea34893cdf7a02f28586efd447c
DESKTOP-HNQ5TBP\lucas <lucas.matias.87@gmail.com>
2018-10-03 17:42:51 -0300

    adição do preparo das disposições

---
+++ AnaliseExploratoriaBerka/PreparoDisposicao.R
@@ -0,0 +1,14 @@
+read.csv2("./dados/disp.asc", stringsAsFactors = FALSE) -> disp
+
+View(disp)
+#disp:
+#Transformar a a coluna type em dummie
+disp$type <- as.factor(disp$type)
+View(disp)
+
+unique(disp$type)
+disp <- disp %>%
+        dplyr::filter(type == "OWNER") %>%
+        select(disp_id, client_id, account_id)
+        
+        
