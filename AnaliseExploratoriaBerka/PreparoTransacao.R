0881a4c60eaa9cd11148d97de40d8a3956f8d22a
DESKTOP-HNQ5TBP\lucas <lucas.matias.87@gmail.com>
2018-10-03 17:18:02 -0300

    Ajustes de transação

--- AnaliseExploratoriaBerka/PreparoTransacao.R
+++ AnaliseExploratoriaBerka/PreparoTransacao.R
@@ -4,10 +4,17 @@ unique(trans$type)
 
 trans <- trans %>%
         dplyr::filter(type != "VYBER") %>%
-        mutate(type = ifelse(type == "PRIJEM", "credito", ifelse(type == "VYDAJ", "debito", type))) %>%
+        mutate(type = ifelse(
+                        type == "PRIJEM", "credito", 
+                          ifelse(type == "VYDAJ", "debito", type))) %>%
         mutate(amount = as.numeric(amount)) %>%
         select(account_id, type, amount) %>%
         group_by(account_id) %>%
-        summarise(credito = sum(ifelse(type == "credito", amount, 0)), debito = sum(ifelse(type == "debito", amount, 0))) %>%
+        summarise(
+          credito = sum(ifelse(type == "credito", amount, 0)),
+          debito = sum(ifelse(type == "debito", amount, 0)),
+          count = n()) %>%
         mutate(withdraw_rate = debito / credito) %>%
-        select(account_id, withdraw_rate)
+        select(account_id, withdraw_rate, count)
+
+View(trans)
